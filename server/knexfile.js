02548575d963eed0007e5c3a8333f0802758579d
Festus <techarenaz01@gmail.com>
2025-11-26 21:40:42 -0800

    add migrate scripts for railway

--- server/knexfile.js
+++ server/knexfile.js
@@ -3,19 +3,25 @@ import "dotenv/config";
 export default {
   development: {
     client: "pg",
-    connection: {
+    connection: process.env.DATABASE_URL || {
       host: process.env.PG_HOST,
-      port: process.env.PG_PORT,
-      database: process.env.PG_DATABASE,
       user: process.env.PG_USER,
       password: process.env.PG_PASSWORD,
+      database: process.env.PG_DATABASE,
+      port: process.env.PG_PORT,
     },
     migrations: {
       tableName: "knex_migrations",
       directory: "./migrations",
     },
-    seeds: {
-      directory: "./seeds",
+  },
+
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      tableName: "knex_migrations",
+      directory: "./migrations",
     },
   },
 };
